64d9d19234a9e72a1a65e58f65cdb31400e95468
jhanack <j.hanack@campus.tu-berlin.de>
2019-12-20 00:23:25 +0100

    cleaning up

--- cc-assignment-2/run-benchmarks5%finished.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "time,cpu,mem,diskRand,diskSeq,fork,uplink" | cat > results-native.csv
-echo "time,cpu,mem,diskRand,diskSeq,fork,uplink" | cat > results-docker.csv
-echo "time,cpu,mem,diskRand,diskSeq,fork,uplink" | cat > results-kvm.csv
-echo "time,cpu,mem,diskRand,diskSeq,fork,uplink" | cat > results-qemu.csv
